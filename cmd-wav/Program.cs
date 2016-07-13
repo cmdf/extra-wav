e42750fa6f093af3cf30a2188738d3acc1406626
Subhajit Sahu <wolfram77@gmail.com>
2016-07-13 17:41:54 +0530

    program: dev & test

--- cmd-wav/Program.cs
+++ cmd-wav/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Media;
 
-namespace cmd_wav {
+namespace orez.owav {
 	class Program {
+
+		// static method
+		/// <summary>
+		/// Duck Hunt is a light gun shooter video game developed and published by
+		/// Nintendo for the Nintendo Entertainment System video game console.
+		/// :: Nintendo, Hiroji Kiyotake (April 21, 1984)
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
 		static void Main(string[] args) {
+			oParams p = new oParams(args);
+			Stream i = p.Input == null ? Console.OpenStandardInput() : (File.Exists(p.Input) ? new FileStream(p.Input, FileMode.Open) : null);
+			if (i == null) { Console.Error.WriteLine("err: Invalid input file \"{0}\".", p.Input); return; }
+			try { (new SoundPlayer(i)).PlaySync(); }
+			catch (Exception e) { Console.Error.WriteLine("err: {0}", e.Message); return; }
 		}
 	}
 }
--- cmd-wav/Properties/AssemblyInfo.cs
+++ cmd-wav/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cmd-wav")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("owav")]
+[assembly: AssemblyDescription("Play PCM WAV audio in Windows Console.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cmd-wav")]
+[assembly: AssemblyCompany("0rez")]
+[assembly: AssemblyProduct("owav")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("89a6197f-ef17-4fb9-8931-48500cecfb5f")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
