cafa7b17fa047df352d974ea685cbe4e117594a8
Subhajit Sahu <wolfram77@gmail.com>
2016-07-13 17:41:54 +0530

    project: program test ok

--- cmd-wav/Program.cs
+++ cmd-wav/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Media;
 
 namespace cmd_wav {
 	class Program {
 		static void Main(string[] args) {
+			oParams p = new oParams(args);
+			Stream i = p.Input == null ? Console.OpenStandardInput() : (File.Exists(p.Input) ? new FileStream(p.Input, FileMode.Open) : null);
+			if (i == null) { Console.Error.WriteLine("err: Invalid input file \"{0}\".", p.Input); return; }
+			try { (new SoundPlayer(i)).PlaySync(); }
+			catch (Exception e) { Console.Error.WriteLine("err: {0}", e.Message); return; }
 		}
 	}
 }
---
+++ cmd-wav/oParams.cs
@@ -0,0 +1,23 @@
+﻿namespace cmd_wav {
+	class oParams {
+
+		// data
+		/// <summary>
+		/// Defines input .wav file path.
+		/// </summary>
+		public string Input;
+
+
+		// constructor
+		/// <summary>
+		/// Get parameters from input arguments.
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
+		public oParams(string[] args) {
+			for (int i = 0, I = args.Length; i < I; i++) {
+				string av = args[i];
+				if (av == "-i" || av == "--input") { Input = ++i < I ? args[i] : ""; }
+			}
+		}
+	}
+}
